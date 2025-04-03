98a879c25fcafbed63c4feeb0f40ad8840b80920
Klecianny Melo <kleciannymelo@gmail.com>
2025-04-03 09:55:25 -0300

    :sparkles: applicationDbContext

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,21 +1,22 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using api.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace api.Data
-{
-    public class ApplicationDBContext : DbContext
-    {
-        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
-        {
-            Stock = Set<StockModel>();
-            Comments = Set<CommentModel>();
-        }
-
-        public DbSet<StockModel> Stock { get; set; }
-        public DbSet<CommentModel> Comments { get; set; }
-    }
-}
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Threading.Tasks;
+ using api.Models;
+ using Microsoft.EntityFrameworkCore;
+ 
+ namespace api.Data
+ {
+     public class ApplicationDBContext : DbContext
+     {
+         public ApplicationDBContext(DbContextOptions dbContextOptions)
+         : base(dbContextOptions)
+         {
+ 
+         }
+ 
+         public DbSet<Stock> Stocks { get; set; }
+ 
+         public DbSet<Comment> Comments { get; set; }
+     }
+ }
