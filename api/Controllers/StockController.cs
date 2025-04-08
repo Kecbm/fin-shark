95e8f81045167908efb5dfb2037cf385c8b14094
Klecianny Melo <kleciannymelo@gmail.com>
2025-04-08 12:49:21 -0300

    :sparkles: stock GET by id

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -24,5 +24,18 @@ namespace api.Controllers
 
             return Ok(stocks);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
     }
 }
