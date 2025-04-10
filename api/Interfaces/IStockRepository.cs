252e59321a80410781b58c150d35ae09a2532eb1
Klecianny Melo <kleciannymelo@gmail.com>
2025-04-10 17:08:31 -0300

    :sparkles: update interface

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using api.Dtos.Stock;
 
 namespace api.Interfaces
 {
@@ -11,5 +12,6 @@ namespace api.Interfaces
         Task<List<Stock>> GetAllAsync();
         Task<Stock?> GetByIdAsync(int id);
         Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
     }
 }
