868493645b9f0936e4b0af856f940c9c7d53770b
Klecianny Melo <kleciannymelo@gmail.com>
2025-04-10 17:10:49 -0300

    :sparkles: delete interface

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -13,5 +13,6 @@ namespace api.Interfaces
         Task<Stock?> GetByIdAsync(int id);
         Task<Stock> CreateAsync(Stock stockModel);
         Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
     }
 }
