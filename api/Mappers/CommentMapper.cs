32ffb2064018668a65d23ef536093556be29f646
Klecianny Melo <kleciannymelo@gmail.com>
2025-04-15 13:33:42 -0300

    :sparkles: comment toCommentToCreate

--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -20,5 +20,15 @@ namespace api.Mappers
                 StockId = commentModel.StockId
             };
         }
+
+        public static Comment ToCommentToCreate(this CreateCommentDto commentDto, int stockId)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+                StockId = stockId
+            };
+        }
     }
 }
